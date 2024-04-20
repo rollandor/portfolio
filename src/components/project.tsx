85979315aa54e5aad24cce0dc84c23c30a2c9325
Vladislav <rolanddoor@gmail.com>
2024-04-20 18:13:23 +0300

    fixed displaying of projects for mobile devices

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -30,12 +30,21 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[26rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg pointer-events-none overflow-hidden sm:pr-8 relative sm:h-[26rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col items-center h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description.en}
           </p>
+          <Image
+            src={imageUrl}
+            alt="Project I worked on"
+            quality={95}
+            className="sm:absolute sm:block sm:py-0 py-4 top-8 -right-40 w-[28.25rem] rounded-lg shadow-2xl
+              transition 
+
+              group-even:right-[initial] group-even:-left-40"
+          />
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
@@ -47,24 +56,6 @@ export default function Project({
             ))}
           </ul>
         </div>
-
-        <Image
-          src={imageUrl}
-          alt="Project I worked on"
-          quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-lg shadow-2xl
-        transition 
-        group-hover:scale-[1.04]
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
-
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:translate-y-3
-        group-even:group-hover:rotate-2
-
-        group-even:right-[initial] group-even:-left-40"
-        />
       </section>
     </motion.div>
   );
